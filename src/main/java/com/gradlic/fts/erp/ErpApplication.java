c29286d79fa1041bf97cadc473bbaafaecfb27e4
Vikas Yadav <v.ray96610@gmail.com>
2023-09-14 00:21:06 +0530

    spring-boot application basic structure

---
+++ src/main/java/com/gradlic/fts/erp/ErpApplication.java
@@ -0,0 +1,23 @@
+package com.gradlic.fts.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+// @SpringBootApplication( exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
+public class ErpApplication {
+	private static final int STRENGTH = 12;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder(STRENGTH);
+	}
+
+}
