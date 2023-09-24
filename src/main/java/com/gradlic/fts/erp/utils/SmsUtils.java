8c35cbe862d5cbc07f3d690433f5b106affbfd96
Vikas Yadav <v.ray96610@gmail.com>
2023-09-24 23:19:18 +0530

    Refactor to return UserPrincipal instead of email in authentication.

--- src/main/java/com/gradlic/fts/erp/utils/SmsUtils.java
+++ src/main/java/com/gradlic/fts/erp/utils/SmsUtils.java
@@ -5,9 +5,9 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SmsUtils {
-    public static final String FROM_NUMBER = "+17178424458";
-    public static final String SID_KEY = "ACaed09f77140052e615b22a0b75d07067";
-    public static final String TOKEN_KEY = "4f939ac17acfe1f3244676c93ff204a5";
+    public static final String FROM_NUMBER = "+#";
+    public static final String SID_KEY = "#";
+    public static final String TOKEN_KEY = "#";
 
     public static void sendSMS(String to, String messageBody){
         System.out.println(to);
